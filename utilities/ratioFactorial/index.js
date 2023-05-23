3044537472461a7c8356039bdf64bbdda3bb4d2c
GokulKollipara <gokulkollipara@gmail.com>
2023-05-23 17:30:14 +0000

    ratio and factorial

---
+++ utilities/ratioFactorial/index.js
@@ -0,0 +1,9 @@
+const factorialOfNumber = require("../factorial/index.js");
+const ratioOfTwoNumbers = require("../ratio/index.js");
+
+const ratioAndFactorial = (num1, num2, num3) => {
+  const factorial = factorialOfNumber(num3);
+  const ratio = ratioOfTwoNumbers(num1, num2);
+  return { factorial, ratio };
+};
+module.exports = ratioAndFactorial;
